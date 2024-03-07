3c18c18a5e8c408d953c3cab81780a0086c9585a
Sidney Cardoso <sidneyrsc342@gmail.com>
2024-03-07 18:14:37 -0300

    Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Button } from './components/Button/main'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export const App = () => {
     return (
         <ThemeProvider theme={defaultTheme}>
+            
             <Button variant='primary'/>
             <Button variant='secondary'/>
             <Button variant='success'/>
             <Button variant='danger'/>
             <Button />
+
+            <GlobalStyle/>
         </ThemeProvider>
         )
 }
