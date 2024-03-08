d690f3115ff3b37a73741c4a5fd3693453a8e40d
Sidney Cardoso <sidneyrsc342@gmail.com>
2024-03-07 21:19:41 -0300

    React Router Dom

---
+++ src/Router.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import { History } from './pages/History';
+import { Home } from './pages/Home';
+
+export const Router = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/history" element={<History />} />
+		</Routes>
+	);
+};
--- src/components/Button/main.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from './styles.ts';
-
-interface ButtonProps {
-    variant?: ButtonVariant;
-}
-
-export const Button = ({ variant = 'primary'}: ButtonProps) => {
-	return <ButtonContainer variant={variant}>Enviar</ButtonContainer>;
-};
