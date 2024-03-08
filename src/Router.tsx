d88b75d1a66328cb01ca7e7179f31874ce26682c
Sidney Cardoso <sidneyrsc342@gmail.com>
2024-03-07 22:33:37 -0300

    Pagina Home

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { DefaultLayout } from './layouts/DefaultLayout';
-import { History } from './pages/History';
-import { Home } from './pages/Home';
+import { History } from './pages/History/index';
+import { Home } from './pages/Home/index';
 
 export const Router = () => {
 	return (
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import { Play } from 'phosphor-react';
+import { CountdownContainer, FormContainer, HomeContainer, Separator } from './styles';
+
+export const Home = () => {
+	return (
+		<HomeContainer>
+			<form action="">
+				<FormContainer>
+					<label htmlFor="task">Vou trabalha em</label>
+					<input type="text" id="task"/>
+
+					<label htmlFor="minutesAmount">durante</label>
+					<input type="number" id="minutesAmount" />
+
+					<span>minutos.</span>
+				</FormContainer>
+
+				<CountdownContainer>
+					<span>0</span>
+					<span>0</span>
+					<Separator>:</Separator>
+					<span>0</span>
+					<span>0</span>
+				</CountdownContainer>
+
+				<button type="submit">
+					<Play size={24}/>
+                    Começar
+				</button>
+			</form>
+		</HomeContainer>
+	);
+};
