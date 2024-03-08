632eaad150fa3b8d9fde33537deb0e572141f6ab
Sidney Cardoso <sidneyrsc342@gmail.com>
2024-03-08 10:59:09 -0300

    Aprimoramento de inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,10 +7,29 @@ export const Home = () => {
 			<form action="">
 				<FormContainer>
 					<label htmlFor="task">Vou trabalha em</label>
-					<TaskInput placeholder="Dê um nome para o seu projeto"type="text" id="task"/>
+					<TaskInput 
+						placeholder="Dê um nome para o seu projeto"
+						type="text" 
+						id="task"
+						list="task-suggestions"
+					/>
+                    
+					<datalist id="task-suggestions">
+						<option value="Projeto 1" />
+						<option value="Projeto 2" />
+						<option value="Projeto 3" />
+						<option value="Projeto 4" />
+					</datalist>
 
 					<label htmlFor="minutesAmount">durante</label>
-					<MinutesAmountInput type="number" id="minutesAmount" placeholder='00' />
+					<MinutesAmountInput 
+						type="number" 
+						id="minutesAmount" 
+						placeholder='00' 
+						step={5}
+						min={5}
+						max={60}
+					/>
 
 					<span>minutos.</span>
 				</FormContainer>
