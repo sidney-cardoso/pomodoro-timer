44af460072d507011744c97a59c7b6e0e3747f20
Sidney Cardoso <sidneyrsc342@gmail.com>
2024-03-19 10:53:55 -0300

    Ciclo completo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,7 +10,7 @@ import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, S
 
 const newCycleFormValidationSchema = zod.object({
 	task: zod.string().min(1, 'Informe a tarefa!'),
-	minutesAmount: zod.number().min(5, 'O ciclo precisa ser de no mínimo 5 minutos!').max(60, 'O ciclo precisa ser de no máximo 60 minutos!'),
+	minutesAmount: zod.number().min(1, 'O ciclo precisa ser de no mínimo 5 minutos!').max(60, 'O ciclo precisa ser de no máximo 60 minutos!'),
 });
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
@@ -21,6 +21,7 @@ interface Cycle {
     minutesAmount: number;
     startDate: Date;
     interruptedDate?: Date;
+    finishedDate?: Date;
 }
 
 export const Home = () => {
@@ -38,19 +39,38 @@ export const Home = () => {
 	});
 
 	const activeCycle = cycles.find(cycle => cycle.id === activeCycleId);
-	let interval: number;
 
+	const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+
+	let interval: number;
+    
 	useEffect(() => {
 		if(activeCycle) {
 			interval = setInterval(() => {
-				setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate));
+				const secondsDifference = differenceInSeconds(new Date(), activeCycle.startDate);
+				
+				if (secondsDifference >= totalSeconds){
+					setCycles( state => state.map(cycle => {
+						if(cycle.id === activeCycleId) {
+							return {
+								...cycle, finishedDate: new Date()
+							};
+						} else {
+							return cycle;
+						}
+					}));
+					setAmountSecondsPassed(totalSeconds);
+					clearInterval(interval);
+				} else {
+					setAmountSecondsPassed(secondsDifference);
+				}
 			}, 1000);
 		}
 
 		return () => {
 			clearInterval(interval);
 		};
-	}, [activeCycle]);
+	}, [activeCycle, totalSeconds, activeCycleId]);
     
 	const handleCreateNewCycle = (data: NewCycleFormData) => {
 		const id = String(new Date().getTime()); 
@@ -69,19 +89,19 @@ export const Home = () => {
     
 	const handleInterruptCycle = () => {
         
-		setCycles(cycles.map(cycle => {
-			if(cycle.id === activeCycleId) {
-				return {
-					...cycle, interruptedDate: new Date()
-				};
-			} else {
-				return cycle;
-			}
-		}));
+		setCycles( state => 
+			state.map(cycle => {
+				if(cycle.id === activeCycleId) {
+					return {
+						...cycle, interruptedDate: new Date()
+					};
+				} else {
+					return cycle;
+				}
+			}));
 		setActiveCycleId(null);
 	};
 
-	const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
 	const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
 
 	const minutesAmount = Math.floor(currentSeconds / 60);
@@ -119,7 +139,7 @@ export const Home = () => {
 						id="minutesAmount" 
 						placeholder='00' 
 						step={5}
-						min={5}
+						min={1}
 						max={60}
 						disabled={!!activeCycle}
 						{...register('minutesAmount', { valueAsNumber: true})}
