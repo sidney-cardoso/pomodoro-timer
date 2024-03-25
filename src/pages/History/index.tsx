b5447ba7f520be4e42f37a4eed2bab589a9d0861
Sidney Cardoso <sidneyrsc342@gmail.com>
2024-03-25 10:09:37 -0300

    Formatação da data

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -2,6 +2,9 @@ import { useContext } from 'react';
 import { CyclesContext } from '../../contexts/CyclesContext';
 import { HistoryContainer, HistoryList, Status } from './styles';
 
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR';
+
 export const History = () => {
 
 	const { cycles } = useContext(CyclesContext);
@@ -25,8 +28,11 @@ export const History = () => {
 							return (
 								<tr key={cycle.id}>
 									<td>{cycle.task}</td>
-									<td>{cycle.minutesAmount} minutos</td>
-									<td>{cycle.startDate.toISOString()}</td>
+									<td>{cycle.minutesAmount}</td>
+									<td>{formatDistanceToNow(cycle.startDate, {
+										addSuffix: true,
+										locale: ptBR
+									})}</td>
 									<td>
 										{cycle.finishedDate && (
 											<Status statusColor='green'>Concluído</Status>
